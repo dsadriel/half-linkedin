f1416b52529e07ca85642ae76d11033b92529573
Carlos Zukov <143737340+zukovCarlos@users.noreply.github.com>
2024-12-19 12:01:29 -0300

    Update tabelas.sql

--- etapa-ii/tabelas.sql
+++ etapa-ii/tabelas.sql
@@ -2,8 +2,13 @@
 
 -- ENTIDADES PRINCIPAIS --
 CREATE TABLE PERFIL (
--- @carlos
-
+    id PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao_longa VARCHAR(3000),
+    localizacao VARCHAR(100) NOT NULL,
+    foto_perfil VARCHAR(255) CHECK (foto_perfil LIKE 'https://%.jpg'),
+    foto_capa VARCHAR(255) CHECK (foto_capa LIKE 'https://%.jpg'),
+    email VARCHAR(100) UNIQUE
 );
 
 CREATE TABLE PERFIL_PESSOAL (
@@ -13,7 +18,9 @@ CREATE TABLE PERFIL_PESSOAL (
 );
 
 CREATE TABLE PERFIL_COMPANHIA (
--- @carlos
+    id INT PRIMARY KEY,
+    botar link aqui
+    FOREIGN KEY (id) REFERENCES PERFIL(id)
 );
 
 
@@ -24,7 +31,8 @@ CREATE TABLE PUBLICACAO (
 );
 
 CREATE TABLE PUBLICACAO_FEED (
--- @carlos
+    id_publi_feed INT PRIMARY KEY,
+    id_publi_feed INT REFERENCES PUBLICACAO(id_publicacao)
 );
 
 
@@ -79,9 +87,22 @@ CREATE TABLE TIPO_EMPREGO (
 
 -- ATRIBUTOS MULTI-VALORADOS --
 CREATE TABLE COMPETENCIA (
--- @carlos
+    id_competencia VARCHAR(50) PRIMARY KEY,
+    competencia VARCHAR(100) NOT NULL,
+    FOREIGN KEY (id_competencia) REFERENCES VAGA
 );
 
+CREATE TABLE FOTOS_PUBLICACAO (
+    id_foto_feed INT PRIMARY KEY,
+    foto_publi VARCHAR(255) CHECK (foto_publi LIKE 'https://%.jpg'),
+    FOREIGN KEY (id_foto_feed) REFERENCES PUBLICACAO_FEED
+);
+
+CREATE TABLE LINKS (
+    id_links INT PRIMARY KEY,
+    links VARCHAR(255) CHECK (links LIKE 'https://%.com.br'),
+    FOREIGN KEY (id_links) REFERENCES EVENTO
+);
 
 -- RELACIONAMENTOS --
 CREATE TABLE CURTIDA (
@@ -94,28 +115,43 @@ CREATE TABLE CURTIDA (
 );
 
 CREATE TABLE SETOR_COMPANHIA (
--- @carlos
+    id_setor VARCHAR (50),
+    id_perfil INT,
+    PRIMARY KEY (id_setor, id_perfil_companhia),
+    FOREIGN KEY (id_setor) REFERENCES LISTA_SETOR(id_setor),
+    FOREIGN KEY (id_perfil) REFERENCES PERFIL_COMPANHIA(id),
 );
 
 
 CREATE TABLE APLICACAO_EM_VAGA (
--- @carlos
+    id_perfil INT ,
+    id_vaga VARCHAR (50),
+    estado VARCHAR (30) NOT NULL,
+    data_inscricao DATE NOT NULL,
+    PRIMARY KEY (id_perfil, id_vaga),
+    FOREIGN KEY (id_perfil) REFERENCES PERFIL_PESSOAL(id),
+    FOREIGN KEY (id_vaga) REFERENCES VAGA(id_vaga),
 );
 
 CREATE TABLE INSCRICAO_EM_EVENTO (
     id_perfil VARCHAR(50),
     id_evento VARCHAR(50),
     PRIMARY KEY (id_perfil, id_evento),
-    FOREIGN KEY (id_perfil) REFERENCES PERFIL(id),
+    FOREIGN KEY (id_perfil) REFERENCES PERFIL_PESSOAL(id),
     FOREIGN KEY (id_evento) REFERENCES EVENTO(id_evento)
 );
 
 CREATE TABLE POSSUI_IDIOMA (
--- @carlos
+    id_perfil INT,
+    nome VARCHAR(50),
+    proficiencia VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_perfil, nome, proficiencia),
+    FOREIGN KEY (id_perfil) REFERENCES PERFIL_PESSOAL(id),
+    FOREIGN KEY (nome, proficiencia) REFERENCES LISTA_IDIOMA(nome, proficiencia)
 );
 
 CREATE TABLE EXPERIENCIA (
-    id_perfil VARCHAR(50),
+    id_perfil INT,
     id_companhia VARCHAR(50),
     titulo VARCHAR(255) NOT NULL,
     data_inicio DATE NOT NULL,
@@ -127,5 +163,10 @@ CREATE TABLE EXPERIENCIA (
 );
 
 CREATE TABLE SEGUIMENTO (
--- @carlos
+    id_perfil INT,
+    id_perfil_pessoal INT,
+    data_seguimento DATE NOT NULL,
+    PRIMARY KEY (id_perfil, id_perfil_pessoal),
+    FOREIGN KEY (id_perfil) REFERENCES PERFIL(id),
+    FOREIGN KEY (id_perfil_pessoal) REFERENCES PERFIL_PESSOAL(id)
 );
